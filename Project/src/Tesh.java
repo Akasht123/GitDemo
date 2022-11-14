27395a203c03e96f40228e537b33f9453c8ac580
Akash <akasht1609@gmail.com>
2022-11-14 15:58:41 -0800

    first commit

---
+++ Project/src/Tesh.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Tesh {
+
+	public static void main(String[] args) {
+	ArrayList<Integer> a = new ArrayList<>();
+	a.add(45);
+	System.out.println(a);
+
+	}
+
+}
